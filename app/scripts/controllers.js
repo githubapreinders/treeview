cdd7ef68a3481fd0ca49a5f6bb1c574d0e2e3260
apreinders <apreinders74@gmail.com>
2018-01-10 22:26:00 +0100

    repairing small errors

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -12,65 +12,30 @@
             vm.message = "Angular Controller is wired up";
             vm.remove = remove;
             vm.toggle = toggle;
-            vm.change = change;
+            vm.setSelectedSlot = setSelectedSlot;
             vm.changeName = changeName;
             vm.newSubitem = newSubitem;
             vm.list = ZipService.init();
+            vm.mySlots = ZipService.getSlots();
+            vm.selectedSlot = "file1";
             var deleted;
-            vm.treeOptions=
+
+
+
+            //is called in home.html
+            vm.treeOptions =
             {
-                removed : function(node)
-                {
-                    console.log("removed node ", node.$modelValue, "\n", vm.list);
-                    $scope.$watch(vm.list,function()
-                    {
-                        if(deleted)
-                        {
-                            console.log("vm.list...",  vm.list);
-                            if(node.$modelValue.isDirectory)
-                            {
-
-                            }
-                            else
-                            {
-                                var array = JSON.stringify(vm.list);
-                                console.log(typeof array);
-                                if (!(traverseArray(array)))
-                                {
-                                    console.log("rejecting deletion...");
-                                    newSubitem(node);
-                                }
-                            }
-                          deleted = false;  
-                        }
-                    });
-
-
-                    
-                }
+                
             };
 
-            function traverseArray(sublist)
+
+
+            function setSelectedSlot(object)
             {
-             //sublist = JSON.parse(sublist);
-             console.log("coming in ", sublist[0].title,sublist[0].nodes,"\n\n",sublist, typeof sublist);   
-                var ok;
-                for(var i = 0 ; i < sublist.length; i++)
-                {
-                    console.log("item",sublist[i].title);
-                    if(!(sublist[i].isDirectory)) 
-                    {
-                        console.log("match", sublist[i]);
-                        ok = true;
-                        break;
-                    }
-                    if(sublist[i].nodes.length > 0)
-                    {
-                        traverseArray(sublist[i].nodes);
-                    }
-                }
-                return ok;
+                vm.selectedSlot = object.$modelValue.id;
+                console.log("setting slot to ", vm.selectedSlot);
             }
+
 
             /* Sets the name of the tree item contenteditable and places the caret; 
             On enter or on blur the nameGiver directive updates the model with the current viewvalue.
@@ -93,10 +58,6 @@
 
             }
 
-            function change()
-            {
-
-            }
 
             /*
                 Removes an item from the tree after checking if the user doesn't check the last item.
@@ -120,13 +81,25 @@
                           if(lookForDirectory(currentLayer.$parentNodesScope.$modelValue, object.$modelValue.id))
                             {
                                 console.log("removable: ", object.$modelValue.title);
-                                object.remove();
+                                var results = deleteContainingFiles(object);
+                                console.log("results: ",results );
+                                
+                                //checking for the case that no files are left after this deletion
+                                //updating the mySlots object with the current slots.
+                                if(results.length < Object.keys(vm.mySlots).length)
+                                {   
+                                    results.forEach(function(item)
+                                    {
+                                        delete vm.mySlots[item];
+                                    });
+                                    object.remove();
+                                }
                                 break;
                             }
                           else
                           {
                             currentLayer = currentLayer.$parentNodeScope;
-                            console.log("watchin layer above" ,currentLayer );
+                            console.log("watching layer above" ,currentLayer );
                           }    
                           counter++;
                     }
@@ -136,8 +109,37 @@
                     if(traverseArray(parking.$parentNodesScope.$modelValue, false))
                     {
                         console.log("removable: ", object.$modelValue.title);
-                        object.remove();
+                        delete vm.mySlots[object.$modelValue.id]; //remove from the slots
+                        object.remove(); //remove from the filetree
                     }
+                }
+
+                function deleteContainingFiles(object)
+                {
+                    var results = [];
+                    console.log("children:", object.$childNodesScope.$modelValue);
+                    var array = object.$childNodesScope.$modelValue;
+
+                    findResults(array);
+
+                    function findResults(sublist)
+                    {
+                        for(var i=0 ; i< sublist.length; i++)
+                        {
+                            if(!(sublist[i].isDirectory))
+                            {
+                                results.push(sublist[i].id);
+                            }
+                            else
+                            {
+                                if(sublist[i].nodes.length > 0)
+                                {
+                                    findResults(sublist[i].nodes);   
+                                }
+                            }
+                        }
+                    }
+                     return results;  
                 }
 
                 function getRoot(obj)
@@ -188,16 +190,12 @@
                 }                 
 
                 
-
-
-
             }
 
             function toggle(item)
             {
                item.toggle();
             }
-
 
             function newSubitem(item)
             {
@@ -216,12 +214,15 @@
                 }
                 else
                 {
+                        var theid = Math.floor(Math.random()*10000);
+                        var thetitle = theitem.title + '.' + (theitem.nodes.length + 1);
                     if(item.$parentNodeScope !== null)
                     {
                         item.$parentNodeScope.$modelValue.nodes.push({
-                            id: Math.floor(Math.random()*10000) ,
-                            title: theitem.title + '.' + (theitem.nodes.length + 1),
+                            id: theid ,
+                            title: thetitle,
                             isDirectory : false,
+                            isLocked : false,
                             nodes: []
                             });
                     }
@@ -232,10 +233,12 @@
                                 id: Math.floor(Math.random()*10000) ,
                                 title: theitem.title + '.' + (theitem.nodes.length + 1),
                                 isDirectory : false,
+                                isLocked : false,
                                 nodes: []
                             });
                         console.log("item:", item);
                     }
+                    vm.mySlots[theid] = {"title" : thetitle,"isLocked": false};
                 }
             }
 
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -14,7 +14,7 @@
                         var text = element.innerHTML;
                         var thelist = scope.vm.list;
                         console.log("text",text, thelist, digit);
-                       traverseArray(thelist);
+                        traverseArray(thelist);
 
                         function traverseArray(sublist)
                         {
@@ -24,8 +24,14 @@
                                 //compares int with a string, so NOT === in the comparator
                                 if(sublist[i].id == digit) 
                                 {
-                                    console.log("match", sublist[i], text);
                                     sublist[i].title = text;
+                                    console.log("match", sublist[i], text, scope.vm.mySlots);
+                                    if(!(sublist[i].isDirectory))
+                                    {
+                                        scope.vm.mySlots[digit].title = text;
+                                    }
+
+                                    element.setAttribute('contentEditable', false);
                                     scope.$apply();
                                     break;
                                 }
@@ -45,7 +51,6 @@
                         {
                             event.preventDefault();
                             var element = document.getElementById('treeitem'+event.target.id.match(/\d+/g));
-                            element.setAttribute('contentEditable', false);
                             element.blur();
                         } 
                     });
@@ -56,66 +61,104 @@
      {
         var thelist = 
         [
-        {
-            "id": 1,
-            "isDirectory": true,
-            "title": "dir1",
-            "nodes": [
-            {
-                "id": 2,
-                "isDirectory": false,
-                "title": "file1",
-                "nodes": []
-            },
-            {
-                "id": 784,
-                "title": "dir2",
-                "isDirectory": true,
-                "nodes": [
-                {
-                    "id": 5057,
-                    "title": "file4",
-                    "isDirectory": false,
-                    "nodes": []
-                }
-                ]
-            },
-            {
-                "id": 1105,
-                "title": "dir3",
-                "isDirectory": true,
-                "nodes": []
-            }
-            ]
-        },
-        {
-            "id": 9480,
-            "title": "dir4",
-            "isDirectory": true,
-            "nodes": [
-            {
-                "id": 1819,
-                "title": "file3",
-                "isDirectory": false,
-                "nodes": []
-            }
-            ]
-        },
-        {
-            "id": 7895,
-            "title": "file2",
+  {
+    "id": 123,
+    "title": "dir1",
+    "isDirectory": true,
+    "nodes": [
+      {
+        "id": 1,
+        "title": "file1",
+        "isDirectory": false,
+        "isLocked": false,
+        "nodes": []
+      }
+    ]
+  },
+  {
+    "id": 145,
+    "isDirectory": true,
+    "isLocked": false,
+    "title": "dir2",
+    "nodes": [
+      {
+        "id": 2,
+        "isDirectory": false,
+        "isLocked": false,
+        "title": "file2",
+        "nodes": []
+      },
+      {
+        "id": 3,
+        "title": "file3",
+        "isDirectory": false,
+        "isLocked": false,
+        "nodes": []
+      },
+      {
+        "id": 6407,
+        "title": "dir3",
+        "isDirectory": true,
+        "nodes": [
+          {
+            "id": 4,
+            "title": "file4",
             "isDirectory": false,
+            "isLocked": false,
             "nodes": []
-        }
-        ];
+          }
+        ]
+      },
+      {
+        "id": 5705,
+        "title": "dir4",
+        "isDirectory": true,
+        "nodes": [
+          {
+            "id": 5,
+            "title": "file5",
+            "isDirectory": false,
+            "isLocked": false,
+            "nodes": []
+          }
+        ]
+      },
+      {
+        "id": 2800,
+        "title": "dir5",
+        "isDirectory": true,
+        "nodes": []
+      },
+      {
+        "id": 2428,
+        "title": "dir6",
+        "isDirectory": true,
+        "nodes": []
+      }
+    ]
+  }
+];
         return {
-            init : init
+            init : init,
+            getSlots : getSlots
         };
 
         function init()
         {
             return thelist;
         }
+
+        function getSlots()
+        {
+            return {"1":{"title":"file1", "isLocked": false},
+            "2":{"title":"file2", "isLocked": false},
+            "3":{"title":"file3", "isLocked": false},
+            "4":{"title":"file4", "isLocked": false},
+            "5":{"title":"file5", "isLocked": false}
+        } ;
+        }
+
+
      });
 }());
 
